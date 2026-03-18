2ddc4ade86274182d8db14b3eaef8dd6f33dbbfc
kk-karthik785 <kk1665@srmist.edu.in>
2026-03-18 14:51:49 +0530

    Add files via upload

---
+++ HelloApp.java
@@ -0,0 +1,10 @@
+public class HelloApp {
+
+    // Main method - Entry point of the program
+    public static void main(String[] args) {
+
+        // Print message to console
+        System.out.println("Hello, World!");
+
+    }
+}
